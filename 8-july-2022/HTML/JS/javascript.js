a785bbf9a90c827d5727aec166e81673f9f113f6
mehtadarshil <108316482+mehtadarshil@users.noreply.github.com>
2022-07-13 10:57:46 +0530

    Add files via upload

---
+++ 8-july-2022/HTML/JS/javascript.js
@@ -0,0 +1,9 @@
+function changefont(){
+    document.getElementById('01').style.fontFamily="myfirstfont";
+}
+function chnagesize(){
+    document.getElementById('01').style.fontSize = "60px";
+}
+function hidefont(){
+    document.getElementById('01').style.display = "none";
+}
